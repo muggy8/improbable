313f68f18dc9339475d384c8a9a748e477346fe1
Muggy Ate <muggy8@gmail.com>
2020-02-12 22:48:01 -0800

    text to speech edited ch memories

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ let htmlTemplate = `
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			img {
+				max-width: 100%;
+				height: auto;
+			}
+		</style>
 	</head>
 	<body>uwu</body>
 </html>
